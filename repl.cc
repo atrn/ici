065ff98d0c0d4a51952c7a737408771e97a0fc3e
atrn <an@atrn.org>
2019-04-25 12:56:51 +1000

    missing newline in repl

--- repl.cc
+++ repl.cc
@@ -242,6 +242,7 @@ struct repl_file {
                 sol_ = true;
                 wantp_ = true;
                 havep_ = false;
+                puts("\n");
             } else {
                 sol_ = false;
                 havep_ = false;
